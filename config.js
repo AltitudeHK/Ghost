0c791254863d888f1fe556b21123184eef4671ba
jyek <justinyek@gmail.com>
2014-05-13 12:14:02 -0700

    Updated url

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://www.blog.altitudehk.com',
+        url: 'http://blog.altitudehk.com',
         mail: {},
         database: {
             client: 'sqlite3',
